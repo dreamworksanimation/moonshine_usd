47118e0f6409e18a8995baf670262e147be454c3
kbeason <kevin.beason@dreamworks.com>
2023-03-22 11:13:45 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '10.4'
+    _version = '10.5'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -100,9 +100,9 @@ sconsTargets = {
 }
 
 requires = [
-    'moonray-13.4',
-    'moonshine-10.4',
-    'scene_rdl2-11.4',
+    'moonray-13.5',
+    'moonshine-10.5',
+    'scene_rdl2-11.5',
 ]
 
 private_build_requires = [
