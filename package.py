ddb5fe48a0b1eb1d994671f5a3d30cf0f5032b32
Ron Woods <ron.woods@dreamworks.com>
2023-05-02 13:25:29 -0700

    MOONRAY-4893 (#57)
    * MOONRAY-4893: Enabled test for continuous-integration pipeline.
    
    * MOONRAY-4893: Switch CI plan build to cmake. Fix spelling mistake. Add comment to name_filter disable.
    
    * MOONRAY-4893: Removed disabled comment as it seems to break things.
    
    * MOONRAY-4893: removed one extra test run.

--- package.py
+++ package.py
@@ -107,6 +107,13 @@ private_build_requires = [
     'ispc-1.14.1.x',
 ]
 
+commandstr = lambda i: "cd build/"+os.path.join(*variants[i])+"; ctest -j $(nproc)"
+testentry = lambda i: ("variant%d" % i,
+                       { "command": commandstr(i),
+                         "requires": ["cmake-3.23"] + variants[i] } )
+testlist = [testentry(i) for i in range(len(variants))]
+tests = dict(testlist)
+
 if build_system is 'cmake':
     def commands():
         prependenv('CMAKE_PREFIX_PATH', '{root}')
