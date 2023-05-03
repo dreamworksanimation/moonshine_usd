5f7d1fa15aff027118d27a2939c02b8aef79a697
mrday <mike.day@dreamworks.com>
2023-05-02 17:15:48 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '10.9'
+    _version = '10.10'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -95,9 +95,9 @@ sconsTargets = {
 }
 
 requires = [
-    'moonray-13.9',
-    'moonshine-10.9',
-    'scene_rdl2-11.9',
+    'moonray-13.10',
+    'moonshine-10.10',
+    'scene_rdl2-11.10',
 ]
 
 private_build_requires = [
