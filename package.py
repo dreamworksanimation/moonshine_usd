7960dfb627e83f1a9af2dde7a6719fd3ebd4d39b
scottc <scott.cegielski@dreamworks.com>
2023-10-25 17:49:51 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '12.1'
+    _version = '12.2'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -86,8 +86,8 @@ sconsTargets = {
 }
 
 requires = [
-    'moonray-15.1',
-    'moonshine-12.1',
+    'moonray-15.2',
+    'moonshine-12.2',
     'scene_rdl2-13.0',
 ]
 
