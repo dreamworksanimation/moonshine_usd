1d5869649a31fc8d0f4844a3cca3005ed32ca4ce
Ron Woods <ron.woods@dreamworks.com>
2024-03-08 07:54:33 -0800

    MOONRAY-5137, MOONRAY-5212: Remove scons build capability. (#67)

--- package.py
+++ package.py
@@ -2,10 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # -*- coding: utf-8 -*-
-import os, sys
-
-unittestflags = (['@run_all', '--unittest-xml', '--no-cache']
-                 if os.environ.get('BROKEN_CUSTOM_ARGS_UNITTESTS') else [])
+import os
 
 name = 'moonshine_usd'
 
@@ -32,18 +29,11 @@ authors = [
 help = ('For assistance, '
         "please contact the folio's owner at: moonbase-dev@dreamworks.com")
 
-if 'scons' in sys.argv:
-    build_system = 'scons'
-    build_system_pbr = 'bart_scons-10'
-else:
-    build_system = 'cmake'
-    build_system_pbr = 'cmake_modules'
-
 variants = [
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'gcc-9.3.x.1', 'usd_core-0.21.8.x.2'],
     ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2021.0', 'gcc-9.3.x.1', 'usd_core-0.21.8.x.2'],
 
-    ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'clang-13', 'gcc-9.3.x.1', 'usd_core-0.21.8.x.2'],
+    ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'clang-13', 'usd_core-0.21.8.x.2'],
 
     ['os-CentOS-7', 'opt_level-optdebug', 'refplat-vfx2021.0', 'gcc-9.3.x.1', 'usd_core-0.22.5.x'],
     ['os-CentOS-7', 'opt_level-debug', 'refplat-vfx2021.0', 'gcc-9.3.x.1', 'usd_core-0.22.5.x'],
@@ -82,16 +72,6 @@ variants = [
 conf_rats_variants = variants[0:2]
 conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
-scons_targets = ['@install'] + unittestflags
-proxy_targets = ['@rdl2proxy', '@install_SConscript']
-
-sconsTargets = {
-    'refplat-vfx2020.3': scons_targets,
-    'refplat-vfx2021.0': scons_targets,
-    'refplat-vfx2022.0': scons_targets,
-    'refplat-vfx2023.0': scons_targets,
-}
-
 requires = [
     'moonray-15.16',
     'moonshine-12.16',
@@ -99,7 +79,7 @@ requires = [
 ]
 
 private_build_requires = [
-    build_system_pbr,
+    'cmake_modules',
     'usd_imaging',
     'cppunit',
     'ispc-1.20.0.x',
@@ -112,23 +92,14 @@ testentry = lambda i: ("variant%d" % i,
 testlist = [testentry(i) for i in range(len(variants))]
 tests = dict(testlist)
 
-if build_system is 'cmake':
-    def commands():
-        prependenv('CMAKE_PREFIX_PATH', '{root}')
-        prependenv('SOFTMAP_PATH', '{root}')
-        prependenv('MOONRAY_DSO_PATH', '{root}/rdl2dso')
-        prependenv('RDL2_DSO_PATH', '{root}/rdl2dso')
-        prependenv('LD_LIBRARY_PATH', '{root}/lib')
-        prependenv('PATH', '{root}/bin')
-        prependenv('MOONRAY_CLASS_PATH', '{root}/coredata')
-else:
-    def commands():
-        prependenv('SOFTMAP_PATH', '{root}')
-        prependenv('MOONRAY_DSO_PATH', '{root}/rdl2dso')
-        prependenv('RDL2_DSO_PATH', '{root}/rdl2dso')
-        prependenv('LD_LIBRARY_PATH', '{root}/lib')
-        prependenv('PATH', '{root}/bin')
-        prependenv('MOONRAY_CLASS_PATH', '{root}/coredata')
+def commands():
+    prependenv('CMAKE_PREFIX_PATH', '{root}')
+    prependenv('SOFTMAP_PATH', '{root}')
+    prependenv('MOONRAY_DSO_PATH', '{root}/rdl2dso')
+    prependenv('RDL2_DSO_PATH', '{root}/rdl2dso')
+    prependenv('LD_LIBRARY_PATH', '{root}/lib')
+    prependenv('PATH', '{root}/bin')
+    prependenv('MOONRAY_CLASS_PATH', '{root}/coredata')
 
 uuid = 'bb291e0f-ce27-445d-b280-320a5f23e9b9'
 
