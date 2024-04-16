723a49fe43bdfb47c5d1f8fbdca1dc99290a6996
jlanz <Jon.Lanz@dreamworks.com>
2024-04-16 15:29:06 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '13.0'
+    _version = '13.1'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -58,8 +58,8 @@ conf_rats_variants = variants[0:2]
 conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 requires = [
-    'moonray-16.0',
-    'moonshine-13.0',
+    'moonray-16.1',
+    'moonshine-13.1',
     'scene_rdl2-14.0',
 ]
 
