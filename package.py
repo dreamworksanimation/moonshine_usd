c82117765e4f642b3f6c8be5ce42edd0d8633306
tkato <toshi.kato@dreamworks.com>
2024-07-02 16:06:37 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '13.12'
+    _version = '13.13'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -60,8 +60,8 @@ conf_rats_variants = variants[0:2]
 conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 requires = [
-    'moonray-16.12',
-    'moonshine-13.12',
+    'moonray-16.13',
+    'moonshine-13.13',
     'scene_rdl2-14.10',
 ]
 
