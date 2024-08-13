48325b445034d5d621c625cd762c94e756eaad7a
mrday <mike.day@dreamworks.com>
2024-08-13 14:08:52 -0700

    Bumped minor version for the release.

--- package.py
+++ package.py
@@ -14,7 +14,7 @@ def version():
     """
     Increment the build in the version.
     """
-    _version = '13.17'
+    _version = '13.18'
     from rezbuild import earlybind
     return earlybind.version(this, _version)
 
@@ -60,9 +60,9 @@ conf_rats_variants = variants[0:2]
 conf_CI_variants = list(filter(lambda v: 'os-CentOS-7' in v, variants))
 
 requires = [
-    'moonray-16.17',
-    'moonshine-13.17',
-    'scene_rdl2-14.11',
+    'moonray-16.18',
+    'moonshine-13.18',
+    'scene_rdl2-14.12',
 ]
 
 private_build_requires = [
