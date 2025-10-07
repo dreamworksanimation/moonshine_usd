a96a1acba01201475905e7c5ee27dc6cc3a82460
Lanz, Jon (NBCUniversal) <jon.lanz@dreamworks.com>
2025-10-07 16:11:08 -0700

    MOONRAY-5773, MOONRAY-5752 VFX Reference Platform updates (#103)
    * Rez-test fixes
    
    * Code review fixes
    
    * Code review fixes
    
    * Code review fixes
    
    * Code review fixes

--- package.py
+++ package.py
@@ -30,17 +30,56 @@ help = ('For assistance, '
         "please contact the folio's owner at: moonbase-dev@dreamworks.com")
 
 variants = [
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'gcc-11.x',       'usd_core-0.23.8.x'],
-    ['os-rocky-9', 'opt_level-debug',    'refplat-vfx2023.1', 'gcc-11.x',       'usd_core-0.23.8.x'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'clang-17.0.6.x', 'usd_core-0.23.8.x'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2023.1', 'gcc-11.x',       'usd_core-0.22.5.x'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2022.0', 'gcc-9.3.x.1',    'usd_core-0.22.5.x'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2024.0', 'gcc-11.x',       'usd_core-0.24.3.x'],
-    ['os-rocky-9', 'opt_level-optdebug', 'refplat-vfx2025.0', 'gcc-11.x',       'usd_core-0.25.5.1.x'],
+    [   # variant 0
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2023.1',
+        'gcc-11.x',
+        'usd_core-0.23.8.x'
+    ],
+    [   # variant 1
+        'os-rocky-9',
+        'opt_level-debug',
+        'refplat-vfx2023.1',
+        'gcc-11.x',
+        'usd_core-0.23.8.x'
+    ],
+    [   # variant 2
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2023.1',
+        'clang-17.0.6.x',
+        'usd_core-0.23.8.x'
+    ],
+    [   # variant 3
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2024.0',
+        'gcc-11.x',
+        'usd_core-0.24.3.x'
+    ],
+    [   # variant 4
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2025.0',
+        'gcc-11.x',
+        'usd_core-0.25.5.1.x'
+    ],
+    [   # variant 5
+        'os-rocky-9',
+        'opt_level-optdebug',
+        'refplat-vfx2022.0',
+        'gcc-9.3.x.1',
+        'usd_core-0.22.5.x'
+    ],
 ]
 
 conf_rats_variants = variants[0:2]
 conf_CI_variants = variants
+
+# Add ephemeral package to each variant.
+for i, variant in enumerate(variants):
+    variant.insert(0, '.moonshine_usd_variant-%d' % i)
 
 requires = [
     'moonray-17.27',
@@ -58,7 +97,13 @@ private_build_requires = [
 commandstr = lambda i: "cd build/"+os.path.join(*variants[i])+"; ctest -j $(nproc)"
 testentry = lambda i: ("variant%d" % i,
                        { "command": commandstr(i),
-                         "requires": ["cmake-3.23"] + variants[i] } )
+                        "requires": ["cmake"],
+                        "on_variants": {
+                            "type": "requires",
+                            "value": [".moonshine_usd_variant-%d" % i],
+                            },
+                        "run_on": "explicit",
+                        }, )
 testlist = [testentry(i) for i in range(len(variants))]
 tests = dict(testlist)
 
