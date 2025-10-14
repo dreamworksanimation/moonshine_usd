64efc7fb076b28bfa41b23c52616733edbd0cf5d
Lanz, Jon (NBCUniversal) <jon.lanz@dreamworks.com>
2025-10-14 13:35:22 -0700

    Add refplat-houdini21.0 variant (#106)

--- package.py
+++ package.py
@@ -68,6 +68,13 @@ variants = [
     [   # variant 5
         'os-rocky-9',
         'opt_level-optdebug',
+        'refplat-houdini21.0',
+        'gcc-11.x',
+        'usd_core-0.25.5.1.x'
+    ],
+    [   # variant 6
+        'os-rocky-9',
+        'opt_level-optdebug',
         'refplat-vfx2022.0',
         'gcc-9.3.x.1',
         'usd_core-0.22.5.x'
